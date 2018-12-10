69107b0dbe5bf5633d6718893f789d95eaf03904
sharathu006 <sharathu006@gmail.com>
2018-12-10 16:49:49 +0530

    everything

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,53 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+package "httpd" do
+  action :install
+end
+
+file '/var/www/html/index.html' do
+  content '<html>
+  <body>
+    <h1>hello world</h1>
+  </body>
+</html>'
+end
+
+template '/etc/httpd/conf/httpd.conf' do
+  source 'httpd.erb'
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
+bash "inline script" do
+user "root"
+code "mkdir -p /var/www/my-site && chown -R root /var/www/my-site"
+not_if do
+File.directory?('/var/www/my-site')
+end
+end 
+
+execute "run a script" do
+user "root"
+command <<-EOF
+useradd apache
+chown -R apache /var/www/my-site
+EOF
+not_if
+end
+
+file '/etc/motd' do
+content "This is a sharath test machine
+hostname:  #{ node['hostname']}
+ipaddress: #{ node['ipaddress']}
+"
+owner 'root'
+group 'root'
+action :create
+end
+
+
+service "httpd" do
+  action [:enable, :start]
+end
--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -4,15 +4,22 @@
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
-
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
+# Checking the httpd service
+if os[:family] == 'redhat'
+  describe service('httpd') do
+    it { should be_installed }
+    it { should be_enabled }
+    it { should be_running }
   end
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+# Checking the ports
+ports = [80, 443]
+ports.each do |port|
+  describe port(port) do
+    it { should be_listening }
+    its('protocols') { should include 'tcp' }
+    its('processes') { should include 'httpd' }
+  end
 end
+
